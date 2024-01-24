193606c0d0347f5f4f267eac725ddfc279135e80
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-01-25 00:19:48 +0700

    Fix table

--- src/features/history/components/CustomTable.tsx
+++ src/features/history/components/CustomTable.tsx
@@ -1,3 +1,4 @@
+import { Flex } from 'antd';
 import React, { ReactNode, CSSProperties } from 'react';
 
 interface CustomizableTableProps {
@@ -9,33 +10,75 @@ interface CustomizableTableProps {
 
 const CustomizableTable: React.FC<CustomizableTableProps> = ({ columns, data, styles, onRowClick }) => {
   return (
-    <table className="table-auto w-full border-separate bg-white border-spacing-y-2" style={styles}>
-      <thead className='bg-white border-b border-pastel-orange'>
-        <tr>
-          {columns.map((column) => (
-            <th key={column.key} className="p-2 text-left">
-              {column.header}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, index) => (
-          <tr key={index} className='border-2 rounded-xl bg-peach' style={{ cursor: onRowClick ? 'pointer' : 'default' }} onClick={() => onRowClick && onRowClick(row, index)}>
+    <div>
+      <Flex className='justify-around border-b border-pastel-orange'>
+  {columns.map((column) => (
+    <span key={column.key} style={{ width: column.width }}>
+      {column.header}
+    </span>
+  ))}
+</Flex>
+<Flex className='flex-col'>
+  {data.map((row, index) => (
+    <div key={index} className='flex rounded-xl bg-peach my-2' style={{ cursor: onRowClick ? 'pointer' : 'default' }} onClick={() => onRowClick && onRowClick(row, index)}>
+      {columns.map((column) => (
+        <div key={column.key} className="p-4" style={{ width: column.width }}>
+          {column.key === 'image' ? (
+                  <img src={row['image']} alt="Image" className="object-cover h-24 w-36 rounded-xl border-2 border-pastel-orange " />
+                ) : (
+                  row[column.key]
+                )}
+        </div>
+      ))}
+    </div>
+  ))}
+</Flex>
+    </div>
+  );
+};
+
+export default CustomizableTable;
+
+{/* <Flex className='flex-col'>
+          {data.map((row, index) => (
+          <div key={index} className='flex  rounded-xl bg-peach my-2' style={{ cursor: onRowClick ? 'pointer' : 'default' }} onClick={() => onRowClick && onRowClick(row, index)}>
             {columns.map((column) => (
-              <td key={column.key} className="p-4">
+              <div key={column.key} className="p-4">
                 {column.key === 'image' ? (
                   <img src={row['image']} alt="Image" className="object-cover h-24 w-36 rounded-xl border-2 border-pastel-orange " />
                 ) : (
                   row[column.key]
                 )}
-              </td>
+              </div>
             ))}
-          </tr>
+          </div>
         ))}
-      </tbody>
-    </table>
-  );
-};
+      </Flex> */}
 
-export default CustomizableTable;
+
+ // <table className="table-auto w-full border-separate bg-white border-spacing-y-2" style={styles}>
+    //   <thead className='bg-white border-b border-pastel-orange'>
+    //     <tr>
+    //       {columns.map((column) => (
+    //         <th key={column.key} className="p-2 text-left">
+    //           {column.header}
+    //         </th>
+    //       ))}
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {data.map((row, index) => (
+    //       <tr key={index} className='border-2 rounded-xl bg-peach' style={{ cursor: onRowClick ? 'pointer' : 'default' }} onClick={() => onRowClick && onRowClick(row, index)}>
+    //         {columns.map((column) => (
+    //           <td key={column.key} className="p-4">
+    //             {column.key === 'image' ? (
+    //               <img src={row['image']} alt="Image" className="object-cover h-24 w-36 rounded-xl border-2 border-pastel-orange " />
+    //             ) : (
+    //               row[column.key]
+    //             )}
+    //           </td>
+    //         ))}
+    //       </tr>
+    //     ))}
+    //   </tbody>
+    // </table>
--- src/features/history/index.tsx
+++ src/features/history/index.tsx
@@ -6,40 +6,13 @@ import CustomizableTable from "./components/CustomTable";
 
 export default function History() {
   const navigate = useNavigate();
-  const [liveHistoryList, setLiveHistoryList] =
-    useState<IHistoryItem[]>(mockHistoryListData);
-  // const columns: ColumnsType<IHistoryRow> = [
-  //   {
-  //     title: "",
-  //     dataIndex: "image",
-  //     key: "_image",
-  //     width: `${(300 / 1124) * 100}%`,
-  //   },
-  //   {
-  //     title: "Name",
-  //     dataIndex: "name",
-  //     key: "_name",
-  //     width: `${(330 / 1124) * 100}%`,
-  //   },
-  //   {
-  //     title: "Creator",
-  //     dataIndex: "creator",
-  //     key: "_creator",
-  //     width: `${(300 / 1124) * 100}%`,
-  //   },
-  //   {
-  //     title: "Last edited",
-  //     dataIndex: "lastEdit",
-  //     key: "_lastEdit",
-  //     width: `${(300 / 1124) * 100}%`,
-  //   },
-  // ];
 
   const columns = [
-    { key: 'image', header: '' },
-    { key: 'name', header: 'Name' },
-    { key: 'creator', header: 'Creator' },
-    { key: 'lastEdit', header: 'Last edited' },
+    { key: 'image', header: '', width: '20%' },
+    { key: 'name', header: 'Name', width: '20%' },
+    { key: 'creator', header: 'Creator', width: '20%' },
+    { key: 'lastEdited', header: 'Last edited', width: '20%' },
+    { key: 'action', header: '', width: '20%' },
   ];
 
   const data : IHistoryItem[] = [
@@ -48,21 +21,24 @@ export default function History() {
         "https://media.discordapp.net/attachments/988486551275200573/1115852890720972883/IMG_9028.jpg?ex=65b08b7c&is=659e167c&hm=62f766768e59e8ac0d4d426ab5120de2278cd0bd7fc33a70f9795ecf3eb8c9c7&=&format=webp&width=1560&height=1170",
       name: "Kittiphon Singchom",
       creator: "You",
-      lastEdit: "10 days ago.",
+      lastEdited: "10 days ago.",
+      action: <span></span>
     },
     {
       image:
         "https://media.discordapp.net/attachments/988486551275200573/1149605252149026816/how-to-install-a-split-jamb-door-step-6.png?ex=65aa22d2&is=6597add2&hm=4f59872bca885805d274a5581790057cd43f694f784184bccc3215a7263519f1&=&format=webp&quality=lossless&width=1872&height=1170",
       name: "Chanikan Singchom",
       creator: "You",
-      lastEdit: "15 days ago.",
+      lastEdited: "15 days ago.",
+      action: <span></span>
     },
     {
       image:
         "https://media.discordapp.net/attachments/988486551275200573/1115852891832463390/IMG_6115.jpg?ex=65b08b7c&is=659e167c&hm=8924224afac46d2d02e9e8eb41418e3ed58c275258d0ca62f6a64e1d13b4189a&=&format=webp&width=878&height=1170",
       name: "Chanikan Suechareon",
       creator: "You",
-      lastEdit: "15 days ago.",
+      lastEdited: "15 days ago.",
+      action: <span></span>
     },
   ];
 
@@ -72,7 +48,7 @@ export default function History() {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col px-6 pt-2">
       <p className="text-2xl">Live History</p>
       <div className="flex">
         <div className="pr-2">
@@ -90,32 +66,6 @@ interface IHistoryItem {
   image: string;
   name: string;
   creator: string;
-  lastEdit: string;
+  lastEdited: string;
+  action: JSX.Element
 }
-
-const mockHistoryListData: IHistoryItem[] = [
-  {
-    id: 1,
-    image:
-      "https://media.discordapp.net/attachments/988486551275200573/1115852890720972883/IMG_9028.jpg?ex=65b08b7c&is=659e167c&hm=62f766768e59e8ac0d4d426ab5120de2278cd0bd7fc33a70f9795ecf3eb8c9c7&=&format=webp&width=1560&height=1170",
-    name: "Kittiphon Singchom",
-    creator: "You",
-    lastEdit: "10 days ago.",
-  },
-  {
-    id: 2,
-    image:
-      "https://media.discordapp.net/attachments/988486551275200573/1149605252149026816/how-to-install-a-split-jamb-door-step-6.png?ex=65aa22d2&is=6597add2&hm=4f59872bca885805d274a5581790057cd43f694f784184bccc3215a7263519f1&=&format=webp&quality=lossless&width=1872&height=1170",
-    name: "Chanikan Singchom",
-    creator: "You",
-    lastEdit: "15 days ago.",
-  },
-  {
-    id: 3,
-    image:
-      "https://media.discordapp.net/attachments/988486551275200573/1115852891832463390/IMG_6115.jpg?ex=65b08b7c&is=659e167c&hm=8924224afac46d2d02e9e8eb41418e3ed58c275258d0ca62f6a64e1d13b4189a&=&format=webp&width=878&height=1170",
-    name: "Chanikan Suechareon",
-    creator: "You",
-    lastEdit: "15 days ago.",
-  },
-];
