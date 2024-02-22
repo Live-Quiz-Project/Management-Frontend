2b1321496c4dec6e87e87083366198ce0b81ea67
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-02-22 22:48:45 +0700

    Add app logo

--- src/common/layouts/auth/components/LeftPanel.tsx
+++ src/common/layouts/auth/components/LeftPanel.tsx
@@ -1,3 +1,5 @@
+import appLogo from "../../../assets/app_logo.png";
+
 type Props = {
   page?: string;
 };
@@ -5,9 +7,15 @@ type Props = {
 export default function LeftPanel({ page = "login" }: Props) {
   return (
     <div
-      className={`h-dscreen ${page === "login" && "bg-koromiko"} ${
-        page === "register" && "bg-denim"
-      } ${(page === "forgot" || page === "reset") && "bg-jordy-blue"}`}
-    ></div>
+      className={`h-dscreen flex items-center justify-center ${
+        page === "login" && "bg-koromiko"
+      } ${page === "register" && "bg-denim"} ${
+        (page === "forgot" || page === "reset") && "bg-jordy-blue"
+      }`}
+    >
+      <div className="flex justify-center items-center w-2/3">
+        <img src={appLogo} alt="App Logo" />
+      </div>
+    </div>
   );
 }
