41298a217590b650607cb272ac5436146cf1be74
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-02-25 17:58:32 +0700

    Change piechart color

--- src/features/history/components/QuestionItem.tsx
+++ src/features/history/components/QuestionItem.tsx
@@ -27,6 +27,7 @@ const QuestionItem: React.FC<QuestionItemProps> = ({
 
   const chartOptions = {
     chartArea: { width: "80%", height: "80%" },
+    colors: ["#FFAAAA", "#FFCA7A", "#C7DAB0", "#C8DAF5", "#DDD1E1"],
     backgroundColor: "#FFFADD",
   };
 
