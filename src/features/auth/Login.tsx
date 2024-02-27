5601040e2653a865e473b79a7052d253d445a12a
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-02-28 00:01:47 +0700

    Add account name and image

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -51,6 +51,7 @@ export default function Login() {
               id: data.id,
               name: data.name,
               email: data.email,
+              image: data.image,
             },
           })
         );
