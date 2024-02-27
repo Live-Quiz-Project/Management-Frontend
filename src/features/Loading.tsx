a8c508bef76a47b75c90fe0cec24a9143ffcf446
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-02-27 23:29:13 +0700

    Add loading animation

--- src/features/Loading.tsx
+++ src/features/Loading.tsx
@@ -1,3 +1,14 @@
-export default function Loading() {
-  return <div>Loading</div>;
+import ReactLoading from "react-loading";
+
+export function Loading() {
+  return (
+    <div className="flex justify-center loading">
+      <ReactLoading
+        type="spinningBubbles"
+        color="#FFCC70"
+        height={100}
+        width={100}
+      />
+    </div>
+  );
 }
