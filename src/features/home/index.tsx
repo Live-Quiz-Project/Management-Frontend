ffa217c9b23c30ae47420ee1c36e9c0fa3919bf7
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-02-28 23:47:10 +0700

    Fix dropdown ui

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -72,22 +72,22 @@ export default function Home() {
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1211589257601355827/article_210424_0.png?ex=65eebf6d&is=65dc4a6d&hm=d917db439475b278b0380848d9ff138a145c26b65539bc074d1fcc0bf1b7b571&=&format=webp&quality=lossless&width=1210&height=712"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4 cursor-pointer hover:border-pastel-orange"
             />
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1211589402606964797/1000_F_451252677_NYrDRHhFQ8bGqCukaHjcvnP7tzhLCfxL.png?ex=65eebf8f&is=65dc4a8f&hm=5b80ca5094251c764fa1480ad5d5c4f8671f6ff22b8caf0f08b16ceedc70ce0d&=&format=webp&quality=lossless&width=1210&height=745"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4 cursor-pointer hover:border-pastel-orange"
             />
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1211589324391448586/main-qimg-d526ae7e6659634754593e815acf764a-lq.png?ex=65eebf7d&is=65dc4a7d&hm=5066edbc09a27fa1ea91fd12a314011f239787fbef0f599a3f504dfcd44bef93&=&format=webp&quality=lossless&width=1324&height=882"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4 cursor-pointer hover:border-pastel-orange"
             />
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1211589324391448586/main-qimg-d526ae7e6659634754593e815acf764a-lq.png?ex=65eebf7d&is=65dc4a7d&hm=5066edbc09a27fa1ea91fd12a314011f239787fbef0f599a3f504dfcd44bef93&=&format=webp&quality=lossless&width=1324&height=882"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover cursor-pointer hover:border-pastel-orange"
             />
           </Flex>
         </div>
@@ -97,22 +97,22 @@ export default function Home() {
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1211589257601355827/article_210424_0.png?ex=65eebf6d&is=65dc4a6d&hm=d917db439475b278b0380848d9ff138a145c26b65539bc074d1fcc0bf1b7b571&=&format=webp&quality=lossless&width=1210&height=712"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4 cursor-pointer hover:border-pastel-orange"
             />
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1205172567317422140/math-102023-1281244731-01.png?ex=65e9dc6b&is=65d7676b&hm=f659b1905ae896142058c562b2bec9e378a95afb7c311f5ae1c701d60f33fafa&=&format=webp&quality=lossless&width=1150&height=770"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4 cursor-pointer hover:border-pastel-orange"
             />
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1205172679750197340/1000_F_280126582_Ig4OLIbbSryXwe2S63aBu2TKY0Bj9WjH.png?ex=65e9dc85&is=65d76785&hm=c25265f67d8a0573daa80b06bfb0618016b940b1283d6994ed2da51f662bcbff&=&format=webp&quality=lossless&width=1157&height=770"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover mr-4 cursor-pointer hover:border-pastel-orange"
             />
             <img
               src="https://media.discordapp.net/attachments/988486551275200573/1205172750059175998/20231222_cpub_bii_header-1703185204173.png?ex=65e9dc96&is=65d76796&hm=e1f6bcba4113e7cdd1831c41d2ed1156b366c54ad4ff01439d0ef9de549b2e89&=&format=webp&quality=lossless&width=1163&height=770"
               alt="Image"
-              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover"
+              className="h-40 w-screen rounded-xl border-2 border-regent-gray object-cover cursor-pointer hover:border-pastel-orange"
             />
           </Flex>
         </div>
