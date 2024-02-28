66b53283e5eaa46bcc687de95ff77853d30244b4
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-02-28 21:45:52 +0700

    Fix some ui

--- src/features/history/index.tsx
+++ src/features/history/index.tsx
@@ -149,7 +149,7 @@ export default function History() {
 
   return (
     <Topbar>
-      <div>
+      <div style={{ maxHeight: "73vh" }}>
         <p className="font-serif text-2xl">Live History</p>
         <div className="pr-2 py-2">
           <SearchField
