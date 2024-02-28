c321e69b8ea9cec004927688e9755ecad482db60
kittiphonsingchom <kittiphonsingchom@gmail.com>
2024-02-29 00:36:19 +0700

    Fix loading popup

--- src/features/Loading.tsx
+++ src/features/Loading.tsx
@@ -2,7 +2,9 @@ import ReactLoading from "react-loading";
 
 export function Loading() {
   return (
-    <div className="flex justify-center loading">
+    <div
+      className={`fixed top-0 left-0 right-0 bottom-0 bg-main-white flex justify-center items-center z-[100] loading`}
+    >
       <ReactLoading
         type="spinningBubbles"
         color="#FFCC70"
